9bdff7cd3796b419e7b160f69981a69eb17891da
Lucas <lucas.charles@kapturecrm.com>
2024-10-04 16:33:14 +0530

    first commit

--- Package.swift
+++ Package.swift
@@ -5,19 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "KapChatLibrary",
+    platforms: [
+            .iOS(.v16) // iOS version target
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KapChatLibrary",
-            targets: ["KapChatLibrary"]),
+            targets: ["KapChatBotSDK"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "KapChatLibrary"),
-        .testTarget(
-            name: "KapChatLibraryTests",
-            dependencies: ["KapChatLibrary"]),
+//        .target(
+//            name: "KapChatLibrary"),
+//        .testTarget(
+//            name: "KapChatLibraryTests",
+//            dependencies: ["KapChatLibrary"]),
+        .binaryTarget(name: "KapChatBotSDK", path: "./Sources/KapChatBotSDK.xcframework")
     ]
 )
